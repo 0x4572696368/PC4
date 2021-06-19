97e07a33f82ba33ddd4745563235d82efbad92c5
0x4572696368 <erichjordyec@gmail.com>
2021-06-19 11:48:57 -0500

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
         appBar: buildAppBar(),
